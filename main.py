d5d56f10347d1ef0e852e50ff7d7c71717801719
Carlos Armando Moreira <carlosmoreira.bji@gmail.com>
2024-10-05 17:29:32 -0300

    feat: add main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+import telebot
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=["nivel"])
+def nivel(message):
+    bot.send_message(message.chat.id, "O nível do rio é de 1.5m")
+
+@bot.message_handler(commands=["grafico"])
+def grafico(message):
+    bot.send_message(message.chat.id, "Exibindo o gráfico, clique aqui para iniciar: /iniciar")
+
+@bot.message_handler(commands=["camera"])
+def camera(message):
+    bot.send_message(message.chat.id, "Exibindo a câmera de monitoramento")
+
+@bot.message_handler(commands=["option1"])
+def option1(message):
+    text = """
+    O que você quer? (Clique em uma opção)
+    /nivel Consultar o nível do rio
+    /grafico Gráfico do nível do rio
+    /camera Acessar a câmera de moitoramento"""
+    bot.send_message(message.chat.id, text)
+
+def check(message):
+    return True
+
+@bot.message_handler(func=check)
+def responder(message):
+    text = """
+    Escolha uma opção para continuar (Clique no item):
+    /option1 Obter informações do rio
+    /option2 Obter informações do clima"""
+    bot.reply_to(message, text)
+
+bot.polling()
