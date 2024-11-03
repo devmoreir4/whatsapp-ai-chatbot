a918e2992526322b41efa7f2e0b1baef789bd657
Carlos Armando Moreira <carlosmoreira.bji@gmail.com>
2024-11-02 21:59:10 -0300

    feat: improving weather menu

--- main.py
+++ main.py
@@ -120,6 +120,25 @@ def plot_daily_river_level(df):
     
     return grafico_path
 
+def get_weather(city):
+    link = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={OPENWEATHER_API_KEY}&lang=pt_br"
+    response = requests.get(link)
+    if response.status_code == 200:
+        data = response.json()
+        descricao = data['weather'][0]['description']
+        temperatura = data['main']['temp'] - 273.15  # Kelvin to Celsius
+        umidade = data['main']['humidity']
+        vento = data['wind']['speed']
+        
+        return {
+            "descricao": descricao,
+            "temperatura": temperatura,
+            "umidade": umidade,
+            "vento": vento
+        }
+    else:
+        return None
+
 @bot.message_handler(commands=["nivel"])
 def nivel(message):
     river_level = get_river_level()
@@ -164,25 +183,7 @@ def opcao1(message):
     /grafico Exibir gráfico do histórico do nível do rio
     /camera Acessar a câmera de monitoramento"""
     bot.send_message(message.chat.id, text)
-
-def get_weather(city):
-    link = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={OPENWEATHER_API_KEY}&lang=pt_br"
-    response = requests.get(link)
-    if response.status_code == 200:
-        data = response.json()
-        descricao = data['weather'][0]['description']
-        temperatura = data['main']['temp'] - 273.15  # Kelvin to Celsius
-        umidade = data['main']['humidity']
-        vento = data['wind']['speed']
-        
-        return {
-            "descricao": descricao,
-            "temperatura": temperatura,
-            "umidade": umidade,
-            "vento": vento
-        }
-    else:
-        return None
+    bot.send_message(message.chat.id, "Clique aqui para voltar: /inicio")
 
 @bot.message_handler(commands=["temperatura"])
 def temperatura(message):
@@ -228,12 +229,13 @@ def opcao2(message):
     /umidade Consultar a umidade do ar
     /vento Consultar a velocidade do vento"""
     bot.send_message(message.chat.id, text)
+    bot.send_message(message.chat.id, "Clique aqui para voltar: /inicio")
 
 def check(message):
     return True
 
 @bot.message_handler(func=check)
-def responder(message):
+def inicio(message):
     text = """
     Escolha uma opção para continuar (Clique no item):
     /opcao1 Obter informações do rio Itabapoana
