9c9afd3a05eecf0f06826b77be0b221dc8ce825d
Carlos Armando Moreira <carlosmoreira.bji@gmail.com>
2025-02-22 17:47:16 -0300

    feat: add rag to bot

--- app.py
+++ app.py
@@ -1,34 +1,42 @@
 from flask import Flask, request, jsonify
+
 from bot.ai_bot import AIBot
 from services.waha import Waha
+
 
 app = Flask(__name__)
 
 
-@app.route("/chatbot/webhook/", methods=["POST"])
+@app.route('/chatbot/webhook/', methods=['POST'])
 def webhook():
     data = request.json
-    print(f"EVENTO RECEBIDO: {data}")
+    chat_id = data['payload']['from']
+    received_message = data['payload']['body']
+    is_group = '@g.us' in chat_id
+
+    if is_group:
+        return jsonify({'status': 'success', 'message': 'Mensagem de grupo ignorada.'}), 200
 
     waha = Waha()
     ai_bot = AIBot()
 
-    chat_id = data["payload"]["from"]
-    received_message = data["payload"]["body"]
-
     waha.start_typing(chat_id=chat_id)
-
-    response = ai_bot.invoke(question=received_message)
-
-    waha.send_message(
-        chat_id=chat_id, 
-        message=response,
+    history_messages = waha.get_history_messages(
+        chat_id=chat_id,
+        limit=10,
     )
-
+    response_message = ai_bot.invoke(
+        history_messages=history_messages,
+        question=received_message,
+    )
+    waha.send_message(
+        chat_id=chat_id,
+        message=response_message,
+    )
     waha.stop_typing(chat_id=chat_id)
 
-    return jsonify({"status": "success"}), 200
+    return jsonify({'status': 'success'}), 200
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
